53f025fa13f37dbfd49b7241360caaf660196316
Steve Witman <stevewitman@gmail.com>
2021-10-19 21:01:07 -0600

    install Firebase and verify connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+    apiKey: 'AIzaSyClzYxjrVew8_gguQikwk7nfk59xb2aAzc',
+    authDomain: 'ngnuggets.firebaseapp.com',
+    projectId: 'ngnuggets',
+    storageBucket: 'ngnuggets.appspot.com',
+    messagingSenderId: '571106357860',
+    appId: '1:571106357860:web:0cb968617b1069b0aba571',
+    measurementId: 'G-D91ZEGXD29',
+  },
 };
 
 /*
