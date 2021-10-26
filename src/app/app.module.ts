587e02c2993ed5257042fc1c1d73dd8cc2d6f654
Steve Witman <stevewitman@gmail.com>
2021-10-25 21:45:41 -0600

    add nav component
      - create core directory with nav component
      - add material imports for core components in the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { NavComponent } from './core/nav/nav.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    NavComponent
   ],
   providers: [],
   bootstrap: [AppComponent],
